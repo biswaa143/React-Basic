89eb527b00653e4efd070b08ea789ae67f89bc4f
biswa143 <barikbiswa143@gmail.com>
2023-02-16 21:52:17 +0530

    passing data through array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import ExpenseItem from "./components/ExpenseItems";
 // import './App.css';
 
 function App() {
+  const expenses = [
+    {
+      title: "Expense items",
+      name: "Biswa",
+      item: "Food : Rs 10",
+      location: "Location : Cuttack",
+    },
+    {
+      title: "Expense items",
+      name: "Amar",
+      item: "Petrol : Rs 100",
+      location: "Location : BBSR (Indian oil petrol pump)",
+    },
+    {
+      title: "Expense items",
+      name: "Manisha",
+      item: "Movies : Rs 200",
+      location: "Location : Puri (INOX)",
+    },
+  ];
   return (
     // <div className="App">
     //   <header className="App-header">
@@ -19,11 +39,27 @@ function App() {
     //       Learn React
     //     </a>
     //   </header>
-      <div>
-        {/* <h2>Let's get started!</h2> */}
-        <ExpenseItem></ExpenseItem>
-      </div>
-  //   </div>
+    <div>
+      {/* <h2>Let's get started!</h2> */}
+      <ExpenseItem
+        name={expenses[0].name}
+        title={expenses[0].title}
+        item={expenses[0].item}
+        location={expenses[0].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        name={expenses[1].name}
+        title={expenses[1].title}
+        item={expenses[1].item}
+        location={expenses[1].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        name={expenses[2].name}
+        title={expenses[2].title}
+        item={expenses[2].item}
+        location={expenses[2].location}
+      ></ExpenseItem>
+    </div>
   );
 }
 
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,24 +1,21 @@
 import "./ExpenseItems.css";
 
-function ExpenseItem() {
-    const expenseTitle = 'Expense items';
-    const expenseItem1 = 'Food : Rs 10';
-    const expenseItem2 = 'Petrol : Rs 100';
-    const expenseItem3 = 'Movies : Rs 200';
-    const LocationOfExpenditure1 = 'Cuttack';
-    const LocationOfExpenditure2 = 'Bbsr';
-    const LocationOfExpenditure3 = 'Puri';
+function ExpenseItem(props) {
 
   return (
     <div>
       <div className="expense-item">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="catagory">
-        <div>{expenseItem1}------({LocationOfExpenditure1})</div>
-        <div>{expenseItem2}------({LocationOfExpenditure2})</div>
-        <div>{expenseItem3}------({LocationOfExpenditure3})</div>
+        <div>{props.name}<br></br>{props.item}<br></br>{props.location}</div>
       </div>
+      {/* <div className="catagory2">
+        <div>{props.item}------{props.location}</div>
+      </div>
+      <div className="catagory3">
+        <div>{props.item}------{props.location}</div>
+      </div> */}
     </div>
   );
 }
